ef61f138d5672c3015db324c7996d7ff621d9f4a
KapilModi656 <kapilmodi.656@gmail.com>
2025-06-30 16:32:59 +0000

    prompt

--- LLM_MNIT_PROJECT/workflow.py
+++ LLM_MNIT_PROJECT/workflow.py
@@ -154,8 +154,8 @@ def final_prompt_node(state: State):
 4. Detect the user's intent (student-level vs. research-level) and adjust your language, format, and depth accordingly.
 5. If no file or link is provided, base your answer on internal knowledge and relevant context from the vector database.
 6. Always respond with a helpful, humble, and structured tone. Use markdown formatting where applicable.
-7. Provide data in markdown format so that markdown can easily render and show it to user
-8. length of your output should range between 2 to 500 words, depending on the complexity of the topic.
+7. Provide data in markdown format so that markdown can easily render and show it to user and use latex and easy to read by user
+8. length of your output should range between 2 to 400 words, depending on the complexity of the topic.
 You were created by Kapil Modi to help MNIT students and faculty learn and teach more effectively.
 
 ---
